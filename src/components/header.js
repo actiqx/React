f0a15e43ff40589b41d16cf0d511b50261a74930
priyatosh <priyatoshpatra@gmail.com>
2018-11-28 15:31:26 +0530

    Init

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,11 @@
-import React, { Component } from 'react';
-import '../components/css/styles.css';
-
-class Header extends Component {
-  state = {
-    keyword: 'Hello'
-  };
-
-  inputChangeHandle = event => {
-    this.setState({
-      keyword: event.target.value
-    });
-  };
-  render() {
-    return (
-      <header>
-        <div className="logo">Logo</div>
-        <input type="text" onChange={this.inputChangeHandle} />
-      </header>
-    );
-  }
-}
+import React from 'react';
+import classes from '../css/styles.module.css';
+const Header = props => {
+  return (
+    <header>
+      <div className={classes.logo}>Logo</div>
+      <input type="text" onChange={props.keywords} />
+    </header>
+  );
+};
 export default Header;
